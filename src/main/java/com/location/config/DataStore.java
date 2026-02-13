1c9c1c8768391e97814c3247433efcf0c0dbff77
Eng-Nanitoo <i20378.etu@iscae.mr>
2026-02-13 03:49:28 +0000

    Adding datastore content

--- src/main/java/com/location/config/DataStore.java
+++ src/main/java/com/location/config/DataStore.java
@@ -1,5 +1,21 @@
 package com.location.config;
 
-public class DataStore {
+import com.location.model.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class DataStore {
+	
+    private static final Map<String, User> users = new ConcurrentHashMap<>();
+    private static final Map<String, Vehicle> vehicles = new ConcurrentHashMap<>();
+    private static final Map<String, Reservation> reservations = new ConcurrentHashMap<>();
+
+    static {
+        User admin = new User("admin", "admin", "ADMIN");
+        users.put("admin", admin);
+    }
+
+    public static Map<String, User> getUsers() { return users; }
+    public static Map<String, Vehicle> getVehicles() { return vehicles; }
+    public static Map<String, Reservation> getReservations() { return reservations; }
 }
